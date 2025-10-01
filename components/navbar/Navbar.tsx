602e802f6ecdc238b78eedf4724856911a105e0c
simonlhopkins <simonlhopkins@gmail.com>
2025-09-30 21:07:23 -0700

    removed unused supabase

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,10 +1,8 @@
 import CreatePostButton from "@/components/navbar/CreatePostButton";
-import {createClient} from "@/lib/supabase/server";
 import {LogoutButton} from "@/components/logout-button";
 import HomeButton from "@/components/navbar/HomeButton";
 
 export default async function Navbar() {
-    const supabase = await createClient();
     return (
         <div className={"w-full flex justify-between p-2 relative"}>
             <HomeButton/>
