f50089895d3184e60397a4c0c1be41868ab70cc3
simonlhopkins <simonlhopkins@gmail.com>
2025-12-14 18:28:31 -0500

    damn

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,6 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.className} antialiased`}>
-        {process.env.GOOGLE_MAPS_API_KEY}
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
